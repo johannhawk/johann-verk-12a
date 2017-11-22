940f7b84a043bf4e2cc9b9ee746c82d9c283ab59
johannhawk <joichanse@live.com>
2017-11-22 14:28:43 +0000

    Update app.py

--- app.py
+++ app.py
@@ -2,5 +2,6 @@ from bottle import *
 from sys import argv
 @route("/")
 def index():
-  return '''Johann er komminn a Heroku'''
-run(app = app,host='0.0.0.0', port=argv[1], debug=True)
+  return """<h1>Johann er komminn a Heroku</h1>"""
+
+run(host='0.0.0.0', port=argv[1])
